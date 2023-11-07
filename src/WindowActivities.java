66abbf06c928eb4e0e2ca61b75f6acc502498318
arjavvirani95 <arjav.virani17@gmail.com>
2023-11-07 13:56:51 -0500

    commiting new windowsactivities file

---
+++ src/WindowActivities.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivities {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chromer.driver", "D:\\SeleniumJars\\chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+		driver.navigate().to("https://rahulshettyacademy.com");
+		Thread.sleep(1000);
+		driver.navigate().back();
+		Thread.sleep(2000);
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.quit();
+
+	}
+
+}
